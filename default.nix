0688ba23789496ce1a5fb1592a8a386410bb857b
Muhammad Kaisar Arkhan <hi@yukiisbo.red>
2021-01-21 22:33:21 +0100

    Add default.nix

---
+++ default.nix
@@ -0,0 +1,16 @@
+{ mkDerivation, base, indents, optparse-applicative, parsec, stdenv
+, text
+}:
+mkDerivation {
+  pname = "cock";
+  version = "0.1.0.0";
+  src = ./.;
+  isLibrary = true;
+  isExecutable = true;
+  libraryHaskellDepends = [ base indents parsec text ];
+  executableHaskellDepends = [
+    base indents optparse-applicative parsec text
+  ];
+  description = "Simplified markup language to author HTML pages";
+  license = stdenv.lib.licenses.publicDomain;
+}
