d668faf795220318d0bcde907d21cfdd5e6523d5
Muhammad Kaisar Arkhan <hi@yukiisbo.red>
2021-01-22 12:20:07 +0100

    Cock.Html: Use unlines when translating document

--- Lib/Cock/Html.hs
+++ Lib/Cock/Html.hs
@@ -43,4 +43,4 @@ translateHtml (HtmlTag tagName attrs child) = mconcat [start, child', end]
 
 -- | Translate a document to proper HTML
 translateDocument :: [Html] -> T.Text
-translateDocument = mconcat . map translateHtml
+translateDocument = T.unlines . map translateHtml
